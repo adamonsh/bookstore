45b9673efc2c44b5eaf758c16be3b1551ae0e6de
Adam Sheikh <adamosh91@gmail.com>
2024-03-03 18:57:30 -0600

    Added HTTP route

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,11 @@ import { PORT } from "./config.js";
 
 const app = express();
 
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(234).send("MERN Bookstore");
+});
+
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
 });
